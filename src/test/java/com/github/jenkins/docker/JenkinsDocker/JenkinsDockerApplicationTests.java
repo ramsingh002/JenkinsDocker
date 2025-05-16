bc86f81d112ab35a962aad68e1618fd9b19b24fa
DELL <ram.singh002@yahoo.com>
2025-05-16 12:57:37 +0530

    first commit

---
+++ src/test/java/com/github/jenkins/docker/JenkinsDocker/JenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jenkins.docker.JenkinsDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
